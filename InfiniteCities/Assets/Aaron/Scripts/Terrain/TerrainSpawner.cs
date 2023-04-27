724f6fcb77e7df484ed4cc8fa0d41374f94beb25
Tearai <77899947+Tearai@users.noreply.github.com>
2023-04-27 15:28:10 +0100

    Auto terrain update

--- InfiniteCities/Assets/Aaron/Scripts/Terrain/TerrainSpawner.cs
+++ InfiniteCities/Assets/Aaron/Scripts/Terrain/TerrainSpawner.cs
@@ -10,6 +10,10 @@ public class TerrainSpawner : MonoBehaviour
     public float spawnInterval = 5f; // the time interval between spawns
     private GameObject spawnedPrefab; // the reference to the spawned prefab
 
+    public float fadeTime = 1f; // the time it takes for the prefab to fade in/out
+
+
+
     void Start()
     {
         InvokeRepeating("SpawnPrefab", 0f, spawnInterval); // invoke the SpawnPrefab method every spawnInterval seconds
@@ -24,14 +28,48 @@ public class TerrainSpawner : MonoBehaviour
             GameObject prefabToSpawn = prefabs[randomIndex]; // select the prefab to spawn
             if (spawnedPrefab != null)
             {
-                Destroy(spawnedPrefab); // destroy the previously spawned prefab
+                StartCoroutine(FadeOutAndDestroy(spawnedPrefab)); // fade out and destroy the previously spawned prefab
             }
             spawnedPrefab = Instantiate(prefabToSpawn, transform.position, Quaternion.identity); // spawn the prefab
             spawnedPrefab.transform.parent = transform; // make the spawned prefab a child of this game object
+            StartCoroutine(FadeIn(spawnedPrefab)); // fade in the spawned prefab
         }
         else
         {
             Debug.LogWarning("No prefabs found in folder: " + prefabFolderName);
+        }
+    }
+
+    private IEnumerator FadeOutAndDestroy(GameObject obj)
+    {
+        // fade out the object
+        float elapsedTime = 0;
+        while (elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(1f, 0f, elapsedTime / fadeTime);
+            Color color = obj.GetComponent<Renderer>().material.color;
+            color.a = alpha;
+            obj.GetComponent<Renderer>().material.color = color;
+            yield return null;
+        }
+
+        // destroy the object
+        Destroy(obj);
+    }
+
+    private IEnumerator FadeIn(GameObject obj)
+    {
+        // fade in the object
+        float elapsedTime = 0;
+        while (elapsedTime < fadeTime)
+        {
+            elapsedTime += Time.deltaTime;
+            float alpha = Mathf.Lerp(0f, 1f, elapsedTime / fadeTime);
+            Color color = obj.GetComponent<Renderer>().material.color;
+            color.a = alpha;
+            obj.GetComponent<Renderer>().material.color = color;
+            yield return null;
         }
     }
 }
---
+++ InfiniteCities/Assets/Aaron/Scripts/TestFade.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestFade : MonoBehaviour
+{
+    public float fadeSpeed = 1.5f;          // Speed that the object will fade in and out.
+
+    private Renderer test;             // Reference to the Renderer component.
+    private Color originalColor;           // The original color of the object.
+    private bool fadingIn = true;          // Whether the object is currently fading in or out.
+
+    void Start()
+    {
+        // Get the Renderer component of the object.
+        test = GetComponent<Renderer>();
+        // Store the original color of the object.
+        originalColor = test.material.color;
+    }
+
+    void Update()
+    {
+        // Calculate the alpha value for the object's color based on the current time.
+        float alpha = Mathf.PingPong(Time.time * fadeSpeed, 1f);
+        // Create a new color with the same RGB values as the original color, but with the alpha set to the calculated value.
+        Color newColor = new Color(originalColor.r, originalColor.g, originalColor.b, alpha);
+        // Set the object's color to the new color.
+        test.material.color = newColor;
+    }
+}
